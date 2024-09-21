7872b8774bd697a966d57d8eefe5b491a90c12ab
Jesse de Wit <witdejesse@hotmail.com>
2024-09-21 22:03:55 +0200

    reorganize redeem logic in logical pieces

--- swapd/src/redeem/monitor.rs
+++ swapd/src/redeem/monitor.rs
@@ -7,6 +7,8 @@ use thiserror::Error;
 use tokio::join;
 use tracing::{debug, error, field, warn};
 
+use crate::chain::Utxo;
+use crate::swap::Swap;
 use crate::{
     chain::{
         ChainClient, ChainError, ChainRepository, ChainRepositoryError, FeeEstimateError,
@@ -97,103 +99,10 @@ where
         }
 
         loop {
-            let utxos = self.chain_repository.get_utxos().await?;
-            let addresses: Vec<_> = utxos.iter().map(|u| u.address.clone()).collect();
-            let swaps = self.swap_repository.get_swaps(&addresses).await?;
-            let mut redeemable_swaps = HashMap::new();
-            for utxo in utxos {
-                let swap = match swaps.get(&utxo.address) {
-                    Some(swap) => swap,
-                    None => continue,
-                };
-
-                let preimage = match swap.preimage {
-                    Some(preimage) => preimage,
-                    None => continue,
-                };
-
-                let entry = redeemable_swaps
-                    .entry(swap.swap.public.address.clone())
-                    .or_insert((swap.swap.clone(), preimage, Vec::new()));
-                entry.2.push(utxo.utxo);
-            }
-
-            let mut utxo_tasks = FuturesUnordered::new();
-            for (_, (swap, preimage, utxos)) in redeemable_swaps {
-                let swap_repository = Arc::clone(&self.swap_repository);
-                utxo_tasks.push(async move {
-                    // TODO: Remove any outpoints that no longer exist in the utxos list.
-                    // TODO: It is perhaps better to save the utxos we tried to redeem in a transaction.
-                    //       That way we avoid pinning ourselves due to RBF'ing with potentially different input sets. 
-                    let utxos: Vec<_> = match swap_repository.get_paid_outpoints(&swap.public.hash).await {
-                        Ok(outpoints) => if outpoints.is_empty() {
-                            warn!(
-                                hash=field::display(swap.public.hash),
-                                "Could not find paid outpoints for paid swap, redeeming all known utxos");
-
-                            // If the outpoint list is empty, claim all utxos to be sure to redeem something.
-                            utxos
-                        } else {
-                            // Take only outputs that are still unspent. If some are skipped, that may be a loss.
-                            utxos.into_iter().filter(|u|outpoints.contains(&u.outpoint)).collect()
-                        },
-                        Err(e) => {
-                            error!(
-                                hash=field::display(swap.public.hash),
-                                "Failed to get paid outpoints for paid swap, redeeming all known utxos: {:?}", e);
-
-                            // If the database call failed, claim all utxos to be sure to redeem something.
-                            utxos
-                        }
-                    };
-                    (swap, preimage, utxos)
-                });
-            }
-
-            let current_height = self.chain_client.get_blockheight().await?;
-
-            let mut redeem_tasks = FuturesUnordered::new();
-            while let Some((swap, preimage, utxos)) = utxo_tasks.next().await {
-                let fee_estimator = Arc::clone(&self.fee_estimator);
-                let wallet = Arc::clone(&self.wallet);
-                let swap_service = Arc::clone(&self.swap_service);
-                let chain_client = Arc::clone(&self.chain_client);
-                let redeem_repository = Arc::clone(&self.redeem_repository);
-                let min_conf_height = match utxos.iter().map(|u| u.block_height).min() {
-                    Some(min_conf_height) => min_conf_height,
-                    None => continue,
-                };
-                let blocks_left = (swap.public.lock_time as i32)
-                    - (current_height.saturating_sub(min_conf_height) as i32);
-                redeem_tasks.push(async move {
-                    let fee_estimate_fut = fee_estimator.estimate_fee(blocks_left);
-                    let address_fut = wallet.new_address();
-                    let (fee_estimate_res, address_res) = join!(fee_estimate_fut, address_fut);
-                    let fee_estimate = fee_estimate_res?;
-                    let destination_address = address_res?;
-                    let redeem_tx = swap_service.create_redeem_tx(
-                        &swap,
-                        &utxos,
-                        &fee_estimate,
-                        current_height,
-                        &preimage,
-                        destination_address.clone(),
-                    )?;
-                    redeem_repository
-                        .add_redeem(&Redeem {
-                            creation_time: SystemTime::now(),
-                            destination_address,
-                            fee_per_kw: fee_estimate.sat_per_kw,
-                            tx: redeem_tx.clone(),
-                        })
-                        .await?;
-                    chain_client.broadcast_tx(redeem_tx).await?;
-                    Ok::<(), RedeemError>(())
-                });
-            }
-
-            while let Some(result) = redeem_tasks.next().await {
-                // TODO: Handle result
+            debug!("starting chain sync task");
+            match self.do_sync().await {
+                Ok(_) => debug!("chain sync task completed succesfully"),
+                Err(e) => error!("chain sync task failed with: {:?}", e),
             }
 
             tokio::select! {
@@ -205,6 +114,123 @@ where
             }
         }
 
+        Ok(())
+    }
+
+    async fn do_sync(&self) -> Result<(), RedeemError> {
+        let utxos = self.chain_repository.get_utxos().await?;
+        let addresses: Vec<_> = utxos.iter().map(|u| u.address.clone()).collect();
+        let swaps = self.swap_repository.get_swaps(&addresses).await?;
+        let mut redeemable_swaps = HashMap::new();
+        for utxo in utxos {
+            let swap = match swaps.get(&utxo.address) {
+                Some(swap) => swap,
+                None => continue,
+            };
+
+            let preimage = match swap.preimage {
+                Some(preimage) => preimage,
+                None => continue,
+            };
+
+            let entry = redeemable_swaps
+                .entry(swap.swap.public.address.clone())
+                .or_insert((swap.swap.clone(), preimage, Vec::new()));
+            entry.2.push(utxo.utxo);
+        }
+
+        let current_height = self.chain_client.get_blockheight().await?;
+
+        let mut tasks = FuturesUnordered::new();
+        for (_, (swap, preimage, utxos)) in redeemable_swaps {
+            tasks.push(self.redeem_swap(swap, preimage, utxos, current_height))
+        }
+
+        while let Some(result) = tasks.next().await {
+            if let Err(e) = result {
+                // TODO: Log which task it was.
+                error!("redeem task errored with: {:?}", e);
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn redeem_swap(
+        &self,
+        swap: Swap,
+        preimage: [u8; 32],
+        utxos: Vec<Utxo>,
+        current_height: u64,
+    ) -> Result<(), RedeemError> {
+        // TODO: Remove any outpoints that no longer exist in the utxos list.
+        // TODO: It is perhaps better to save the utxos we tried to redeem in a transaction.
+        //       That way we avoid pinning ourselves due to RBF'ing with potentially different input sets.
+        let utxos: Vec<_> = match self
+            .swap_repository
+            .get_paid_outpoints(&swap.public.hash)
+            .await
+        {
+            Ok(outpoints) => {
+                if outpoints.is_empty() {
+                    warn!(
+                        hash = field::display(swap.public.hash),
+                        "Could not find paid outpoints for paid swap, redeeming all known utxos"
+                    );
+
+                    // If the outpoint list is empty, claim all utxos to be sure to redeem something.
+                    utxos
+                } else {
+                    // Take only outputs that are still unspent. If some are skipped, that may be a loss.
+                    utxos
+                        .into_iter()
+                        .filter(|u| outpoints.contains(&u.outpoint))
+                        .collect()
+                }
+            }
+            Err(e) => {
+                error!(
+                    hash = field::display(swap.public.hash),
+                    "Failed to get paid outpoints for paid swap, redeeming all known utxos: {:?}",
+                    e
+                );
+
+                // If the database call failed, claim all utxos to be sure to redeem something.
+                utxos
+            }
+        };
+
+        if utxos.is_empty() {
+            return Ok(());
+        }
+
+        // NOTE: This unwrap only works because the utxos vec is not empty!
+        let min_conf_height = utxos.iter().map(|u| u.block_height).min().unwrap();
+        let blocks_left = (swap.public.lock_time as i32)
+            - (current_height.saturating_sub(min_conf_height) as i32);
+        let fee_estimate_fut = self.fee_estimator.estimate_fee(blocks_left);
+        let address_fut = self.wallet.new_address();
+        let (fee_estimate_res, address_res) = join!(fee_estimate_fut, address_fut);
+        let fee_estimate = fee_estimate_res?;
+        let destination_address = address_res?;
+        // TODO: Rebroadcast the old tx if the fee is sufficient.
+        let redeem_tx = self.swap_service.create_redeem_tx(
+            &swap,
+            &utxos,
+            &fee_estimate,
+            current_height,
+            &preimage,
+            destination_address.clone(),
+        )?;
+        self.redeem_repository
+            .add_redeem(&Redeem {
+                creation_time: SystemTime::now(),
+                destination_address,
+                fee_per_kw: fee_estimate.sat_per_kw,
+                tx: redeem_tx.clone(),
+            })
+            .await?;
+        self.chain_client.broadcast_tx(redeem_tx).await?;
         Ok(())
     }
 }
