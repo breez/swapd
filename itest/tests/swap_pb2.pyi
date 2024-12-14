11386d35da53142ce7d9acc50ce103ef52d8ff28
Jesse de Wit <witdejesse@hotmail.com>
2024-12-14 11:30:43 +0100

    add swap parameters

--- itest/tests/swap_pb2.pyi
+++ itest/tests/swap_pb2.pyi
@@ -1,6 +1,11 @@
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
+from typing import (
+    ClassVar as _ClassVar,
+    Mapping as _Mapping,
+    Optional as _Optional,
+    Union as _Union,
+)
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -15,18 +20,21 @@ class CreateSwapRequest(_message.Message):
     ) -> None: ...
 
 class CreateSwapResponse(_message.Message):
-    __slots__ = ("address", "claim_pubkey", "lock_height")
+    __slots__ = ("address", "claim_pubkey", "lock_height", "parameters")
     ADDRESS_FIELD_NUMBER: _ClassVar[int]
     CLAIM_PUBKEY_FIELD_NUMBER: _ClassVar[int]
     LOCK_HEIGHT_FIELD_NUMBER: _ClassVar[int]
+    PARAMETERS_FIELD_NUMBER: _ClassVar[int]
     address: str
     claim_pubkey: bytes
     lock_height: int
+    parameters: SwapParameters
     def __init__(
         self,
         address: _Optional[str] = ...,
         claim_pubkey: _Optional[bytes] = ...,
         lock_height: _Optional[int] = ...,
+        parameters: _Optional[_Union[SwapParameters, _Mapping]] = ...,
     ) -> None: ...
 
 class PaySwapRequest(_message.Message):
@@ -67,4 +75,39 @@ class RefundSwapResponse(_message.Message):
         self,
         pub_nonce: _Optional[bytes] = ...,
         partial_signature: _Optional[bytes] = ...,
+    ) -> None: ...
+
+class SwapParameters(_message.Message):
+    __slots__ = (
+        "lock_time",
+        "max_swap_amount_sat",
+        "min_swap_amount_sat",
+        "min_utxo_amount_sat",
+    )
+    LOCK_TIME_FIELD_NUMBER: _ClassVar[int]
+    MAX_SWAP_AMOUNT_SAT_FIELD_NUMBER: _ClassVar[int]
+    MIN_SWAP_AMOUNT_SAT_FIELD_NUMBER: _ClassVar[int]
+    MIN_UTXO_AMOUNT_SAT_FIELD_NUMBER: _ClassVar[int]
+    lock_time: int
+    max_swap_amount_sat: int
+    min_swap_amount_sat: int
+    min_utxo_amount_sat: int
+    def __init__(
+        self,
+        lock_time: _Optional[int] = ...,
+        max_swap_amount_sat: _Optional[int] = ...,
+        min_swap_amount_sat: _Optional[int] = ...,
+        min_utxo_amount_sat: _Optional[int] = ...,
+    ) -> None: ...
+
+class SwapParametersRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class SwapParametersResponse(_message.Message):
+    __slots__ = ("parameters",)
+    PARAMETERS_FIELD_NUMBER: _ClassVar[int]
+    parameters: SwapParameters
+    def __init__(
+        self, parameters: _Optional[_Union[SwapParameters, _Mapping]] = ...
     ) -> None: ...
